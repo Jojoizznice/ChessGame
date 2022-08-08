cfd692bb9d50f5ca115dcca6b370a15161aacec5
Johannes <johnyri@outlook.com>
2022-08-08 13:18:31 +0200

    Projektdateien hinzufügen.

---
+++ Console/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Console;
+
+internal class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        System.Console.WriteLine("Hello, World!"); 
+        UI.MainWindow mainWindow = new();
+        mainWindow.ShowDialog();
+    }
+}
