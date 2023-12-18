831b2e944952705db90b7c110de06863cf60a3e2
Johannes Riedmayer <johannes.riedmayer@googlemail.com>
2023-12-18 19:33:44 +0100

    new drawing method for board

---
+++ UI/BoardPrinter.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace UI;
+
+partial class MainWindow
+{
+    void DrawBoard() 
+    {
+        SolidColorBrush dark = new(Color.FromArgb(0xFF, 0x5F, 0x35, 0x35));
+        SolidColorBrush bright = new(Color.FromArgb(0xFF, 0xEE, 0xC8, 0x97));
+
+        for (int i = 0; i < 64; i++)
+        {
+            Rectangle rect = new()
+            {
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Stretch,
+                Stroke = GetBrightDark(i) ? dark : bright,
+                Fill = GetBrightDark(i) ? dark : bright,
+                Visibility = Visibility.Visible,
+
+            };
+
+            ChessGrid.Children.Add(rect);
+            Grid.SetColumn(rect, ( i / 8 ) + 2);
+            Grid.SetRow(rect, ( i % 8 ) + 2);
+        }
+
+        Panel.SetZIndex(rook, 1);
+    }
+
+    static bool GetBrightDark(int position)
+    {
+        int row = position / 8;
+        int modifier = row % 2;
+        int column = position % 8;
+
+        return (column + modifier) % 2 == 0;
+    }
+}
